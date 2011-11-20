05c9fbfbd353d296246430d95d06aa9c83e318d2
Joseph Reisinger <joeraii@cs.utexas.edu>
2011-11-20 13:03:02 -0600

    code cleanup and general release-readiness

---
+++ README.md
@@ -0,0 +1,43 @@
+# dist_lda
+
+Lightweight python implementation of a distributed, collapsed gibbs sampler for
+LDA. Uses redis to coordinate multiple nodes.
+
+dist_lda uses a dirty transaction model where words might be missing from local
+counts and totals in order to improve performance at the expense of further
+approximating the markov chain. I'm confident that there is a convergence
+proof, but its too large to fit in the margin of this README.
+
+
+## TODO:
+-- Model dumping subscription service
+-- Shard topics over multiple redis servers (redis_cluster?)
+-- enumerate strings or otherwise low-bit hash to reduce mem footprint
+-- invert topic->word hashes to be word->topic . This way each word string is only stored once in redis, at the cost of significantly more pipelining
+-- massive amount of benchmarking
+
+## PERF:
+-- The current performance bottleneck seems to be the redis server, since a ton of information is being swapped around. Anecdotally I've found one master can coordinate up to ~20 model shards before performance starts to degrade. Current work is to distribute the model across multiple redii (say, hashed by topic).
+
+
+## BUGS:
+-- If individual processes die and restart, you'll get duplicate zombie words in the global state; fixing this would require significant re-architecting and would probably be too slow
+
+
+
+## LICENSE
+
+   Copyright 2011 Joseph Reisinger
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
--- dist_lda.py
+++ dist_lda.py
@@ -1,173 +1,14 @@
 import sys
-import redis
 import random
 import heapq
 from math import log
 from collections import defaultdict
 from itertools import izip
 
+from redis_model_cache import RedisLDAModelCache
 from lda_utils import *
 
 # XXX: TODO: fix V 
-
-class RedisLDAModelCache:
-    """
-    Holds the current assumed global state and the current local deltas 
-    to the LDA model.
-
-    Currently this holds the entire local model state and can do the sync.
-    """
-    def __init__(self, options):
-        # Get redis host and port
-        try:
-            host, port = options.redis.split(':')
-        except:
-            host = options.redis
-            port = 6379
-
-        sys.stderr.write('connecting to host %s:%d\n' % (host, int(port)))
-        self.r = redis.StrictRedis(host=host, port=int(port), db=options.redis_db)
-
-        self.topics = options.topics
-
-        # Store some metadata
-        self.r.set('model', 'lda')
-        self.r.set('topics', options.topics)
-        self.r.set('alpha', options.alpha)
-        self.r.set('beta', options.beta)
-        self.r.set('document', options.document)
-
-        # Track the local model state
-        self.topic_d = defaultdict(lambda: defaultdict(int))
-        self.topic_w = defaultdict(lambda: defaultdict(int))
-        self.topic_wsum = defaultdict(float)
-
-        # Also track the deltas of the stuff we want to sync
-        self.delta_topic_d = defaultdict(lambda: defaultdict(int))
-        self.delta_topic_w = defaultdict(lambda: defaultdict(int))
-        self.delta_topic_wsum = defaultdict(int)
-
-        self.documents = []
-
-        self.resample_count = 0
-
-    @timed
-    def push_local_state(self):
-        """
-        Push our current set of deltas to the server
-        """
-        # sys.stderr.write('Push local state...\n')
-        with execute(self.r) as pipe:
-            # Update document state from deltas
-            for z,v in self.delta_topic_d.iteritems():
-                for d, delta in v.iteritems():
-                    if self.topic_d[z][d] == 0:  # This works because we're document sharding
-                        pipe.zrem(('d', z), d)
-                    else:
-                        pipe.zincrby(('d', z), d, delta)
-
-            # Update topic state
-            for z,v in self.delta_topic_w.iteritems():
-                for w, delta in v.iteritems():
-                    if delta != 0:
-                        pipe.zincrby(('w', z), w, delta)
-            # Update sums
-            for z, delta in self.delta_topic_wsum.iteritems():
-                if delta != 0:
-                    pipe.hincrby('wsum', z, delta)
-
-
-        # Reset the deltas
-        self.delta_topic_d = defaultdict(lambda: defaultdict(int))
-        self.delta_topic_w = defaultdict(lambda: defaultdict(int))
-        self.delta_topic_wsum = defaultdict(int)
-
-    @timed
-    def pull_global_state(self):
-        # Note we don't need to pull the d state, since our shard is 100% responsible for it
-
-        # XXX: always push the local state first, otherwise we'll end up with inconsistencies
-        self.push_local_state()
-
-        self.topic_w = defaultdict(lambda: defaultdict(int))
-        self.topic_wsum = defaultdict(int)
-
-        # Pull down the global state
-        with transact(self.r) as pipe:
-            # test using remrangebyscore(0,0) and then revrangebyscore(-inf, inf)
-            for z in range(self.topics):
-                pipe.zremrangebyscore(('w', z), 0, 0).execute()
-            # Also count that we're not losing any data
-            for z in range(self.topics):
-                # This is clever because it avoids sending the zeros over the wire
-                pipe.zrevrangebyscore(('w', z), float('inf'), 1, withscores=True)
-            for z, zz in enumerate(pipe.execute()):
-                self.topic_w[z] = defaultdict(int)
-                for w,v in zz:
-                    v = int(v)
-                    assert v > 0
-                    self.topic_w[z][w] = v
-
-            self.topic_wsum = defaultdict(int, {int(k):int(v) for k,v in pipe.hgetall('wsum').execute()[0].iteritems()})
-
-    
-    @staticmethod
-    def topic_to_string(topic, max_length=20):
-        result = []
-        for w,c in topic.iteritems():
-            if len(result) > max_length:
-                heapq.heappushpop(result, (c,w))
-            else:
-                heapq.heappush(result, (c,w))
-        return heapq.nlargest(max_length, result)
-
-    """
-    # @timed
-    def get_d(self, d):
-        assert False
-        topic_d = defaultdict(float)
-        for z,v in self.r.hgetall(('d', d.name)).iteritems():
-            topic_d[z] = float(v)
-        return topic_d
-    """
-
-    def add_d_w(self, d, w, z=None):
-        """
-        Add word w to document d
-        """
-        d.assignment.append(z)
-
-        self.topic_d[z][intern(d.name)] += 1
-        self.topic_w[z][intern(w)] += 1
-        self.topic_wsum[z] += 1
-
-        self.delta_topic_d[z][intern(d.name)] += 1
-        self.delta_topic_w[z][intern(w)] += 1
-        self.delta_topic_wsum[z] += 1
-
-    def move_d_w(self, w, d, i, oldz, newz):
-        """
-        Move w from oldz to newz
-        """
-        if newz != oldz:
-            self.topic_d[oldz][intern(d.name)] += -1
-            self.topic_w[oldz][intern(w)] += -1
-            self.topic_wsum[oldz] += -1
-
-            self.delta_topic_d[oldz][intern(d.name)] += -1
-            self.delta_topic_w[oldz][intern(w)] += -1
-            self.delta_topic_wsum[oldz] += -1
-
-            self.topic_d[newz][intern(d.name)] += 1
-            self.topic_w[newz][intern(w)] += 1
-            self.topic_wsum[newz] += 1
-
-            self.delta_topic_d[newz][intern(d.name)] += 1
-            self.delta_topic_w[newz][intern(w)] += 1
-            self.delta_topic_wsum[newz] += 1
-
-            d.assignment[i] = newz
-
 
 class DistributedLDA:
     def __init__(self, options):
@@ -235,6 +76,7 @@ class DistributedLDA:
             if iter % options.sync_every == 0:
                 self.model.pull_global_state()
             self.do_iteration(iter)
+            self.model.finalize_iteration(iter)
         return self
 
     @timed
